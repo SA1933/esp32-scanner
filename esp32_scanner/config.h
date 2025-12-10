9a9e760f2fbdc9ac4c4588c0a2313458e124146f
Ariq Rasya Eka Maulana <rasyaariq1933@gmail.com>
2025-12-11 02:31:56 +0700

    Update config.h

--- esp32_scanner/config.h
+++ esp32_scanner/config.h
@@ -1,23 +1,35 @@
 #ifndef CONFIG_H
 #define CONFIG_H
 
-#define LCD_I2C_ADDR 0x27
-#define LCD_COLS 20
-#define LCD_ROWS 4
+const char* AP_SSID = "ESP-Scanner";
+const char* AP_PASSWORD = "";
 
-#define SCANNER_RX_PIN 16
-#define SCANNER_TX_PIN 17
+#define LCD_SDA 21
+#define LCD_SCL 22
+#define LCD_ADDRESS 0x27
+
+#define SCANNER_RX 16
+#define SCANNER_TX 17
 #define SCANNER_BAUD 9600
 
-#define AP_SSID "ESP32-Scanner-Config"
-#define AP_PASSWORD "12345678"
-#define WIFI_CONNECT_TIMEOUT 20000
-#define WIFI_RECONNECT_DELAY 5000
+#define KEYPAD_R1 13
+#define KEYPAD_R2 12
+#define KEYPAD_R3 14
+#define KEYPAD_R4 27
+#define KEYPAD_C1 26
+#define KEYPAD_C2 25
+#define KEYPAD_C3 33
+#define KEYPAD_C4 32
 
-#define MAX_RETRIES 3
-#define RETRY_DELAY 2000
-#define HTTP_TIMEOUT 10000
+#define WIFI_TIMEOUT 6000  
+#define KEYPAD_DEBOUNCE 200
+#define DISPLAY_DELAY 1000 
 
-#define QUANTITY_TIMEOUT 30000
+const char* GAS_URL = "-PUT YOUR APP SCRIPT HERE-";
+
+const char* API_KEY = "";
+
+#define LCD_COLS 20
+#define LCD_ROWS 4
 
 #endif
